41ddb00eece79bcb981f22ebe8959cf41ee72107
Vanessa <vanessadonadelli2014@hotmail.com>
2025-03-21 10:11:48 -0300

    entity da tabela cliente criada

---
+++ backend/src/main/java/com/comerciosa/backend/entity/cliente.java
@@ -0,0 +1,73 @@
+package com.comerciosa.backend.entity;
+
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false, length = 14)
+    private String cpf;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private Date dataNascimento;
+    
+    private String endereco;
+
+// getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    } 
+
+    
+}
