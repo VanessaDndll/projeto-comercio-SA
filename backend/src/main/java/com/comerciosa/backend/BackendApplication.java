16b8be0fcb4fd3a5a96ee25a6292cecff54ae2cd
Vanessa <vanessadonadelli2014@hotmail.com>
2025-03-20 22:14:37 -0300

    iniciando mapping e a parte do controller

--- backend/src/main/java/com/comerciosa/backend/BackendApplication.java
+++ backend/src/main/java/com/comerciosa/backend/BackendApplication.java
@@ -2,9 +2,30 @@ package com.comerciosa.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+	-Modificadores de acesso-
+	public, protected, (default), private
+	public -> pode acessar de qualquer lugar
+	(default) -> 
+ */
 
+// API REST
+
+@RestController
 @SpringBootApplication
 public class BackendApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello world!";
+	}
+
+	@RequestMapping("/cliente")
+	String listarClientes() {
+		return "<h1>Listando clientes...</h1>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
